b6ecaf654bcda95b9264fd902ecac3b1c06c858b
Nora Graves <eg5817@princeton.edu>
2023-12-05 10:36:21 -0500

    B attack?

--- createdataB.c
+++ createdataB.c
@@ -3,22 +3,25 @@
 /*--------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     /* create and open file dataB */
+    int i;
     char *names = "Nora and Teddy";
     FILE *psFile;
     psFile = fopen("dataB", "w");
 
     /* put names into file */
-    for (int i = 0; i < strlen(names); i++) {
+    for (i = 0; i < strlen(names); i++) {
         fprintf(psFile, "&c", names[i]);
     }
     fprintf(psFile, "%c", '\0'); /* terminating null byte */
 
     /* fill remaining buffer */
-    for (int i = 0; i < (48 - strlen(names) -1); i++) {
+    for (i = 0; i < (48 - strlen(names) -1); i++) {
         fprintf(psFile, "%c", '\0');
     }
 
+    return 0;
 }
